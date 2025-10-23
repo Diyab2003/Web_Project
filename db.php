37bd761dcf42dd562a7d46b92dbb71f654eb520f
Diya Benny <132542534+Diyab2003@users.noreply.github.com>
2025-10-23 19:40:55 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,13 @@
 <?php
 $servername = "localhost";
-$username = "root";     
-$password = "";         
-$database = "student_db";  
+$username = "root";
+$password = ""; 
+$dbname = "movie_catalog"; 
 
-$conn = mysqli_connect($servername, $username, $password, $database);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 ?>
+

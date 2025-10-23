c8deb4eec8e7b70c0991feddcb73570fe7946ccb
Diya Benny <132542534+Diyab2003@users.noreply.github.com>
2025-10-23 18:37:26 +0530

    Update create.php

--- create.php
+++ create.php
@@ -5,7 +5,8 @@ $sql = "CREATE TABLE IF NOT EXISTS students (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     age INT NOT NULL,
-    course VARCHAR(50) NOT NULL
+    course VARCHAR(50) NOT NULL,
+    branch VARCHAR(50) NOT NULL
 )";
 
 if (mysqli_query($conn, $sql)) {
@@ -16,3 +17,4 @@ if (mysqli_query($conn, $sql)) {
 
 mysqli_close($conn);
 ?>
+
